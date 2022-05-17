a9e4e4979574a0eb560afd45b516f3055bae9d49
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2022-05-17 14:01:11 +0530

    redis db connected

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 const url = require('url');
-const express = require('express')
-const needle = require('needle')
+const redis = require('redis');
+const express = require('express');
+const needle = require('needle');
 const router = express.Router();
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY = process.env.API_KEY;
+
+
+const client = redis.createClient();
+
+// client.on('error', (err) => console.log('Redis Client Error', err));
+
+// await client.connect();
+
+
+
+const fun = async () =>{
+    client.on('error', (err) => console.log('Redis Client Error', err))
+    await client.connect();
+    await client.set('key', 'value');
+    const value = await client.get('key');
+    
+    console.log(value);
+}
+
+fun();
 
 router.get('/',async (req,res)=>{
 
